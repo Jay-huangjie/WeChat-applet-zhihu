83fba10c0bade9ff2dffd7722ea0e32c91878805
jmAndroid <jay@huangjie.name>
2018-12-11 17:11:55 +0800

    tab界面

--- pages/discovery/discovery.js
+++ pages/discovery/discovery.js
@@ -1,4 +1,5 @@
 // pages/discovery/discovery.js
+var util = require('../../utils/util.js')
 Page({
 
   /**
@@ -6,13 +7,58 @@ Page({
    */
   data: {
     navigaTab:["推荐","圆桌","热门","收藏"],
-    currentInde:0
+    currentIndex:0,
+    // screen_h:0, //屏幕高度
+    imgUrls: [
+      '../../images/24213.jpg',
+      '../../images/24280.jpg',
+      '../../images/1444983318907-_DSC1826.jpg'
+    ],
+    feed: [], //数据源
+  },
+  onLoad(){
+    // let that = this;
+    // wx.getSystemInfo({
+    //   success(res) {
+    //     var height = res.windowHeight; //从api中获取屏幕高度
+    //     console.log(height)
+    //     that.setData({
+    //       screen_h:res.windowHeight
+    //     })
+    //   }
+    // });
+    this.getData();
   },
   tabCheck:function(e){
      console.info(e);
      var index = e.target.dataset.index;
      this.setData({
-       currentInde:index
+       currentIndex:index
      })
   },
+  lower:function(){
+    console.info("lower")
+  },
+  upper:function(){
+    console.info("upper")
+  },
+  getData: function () {
+    var feed = util.getData2();
+    this.setData({
+      feed: feed.data,
+    })
+  },
+  toAnswer: function () {
+    wx.navigateTo({
+      url: '../answer/answer',
+    })
+  },
+  toQuestion: function () {
+    wx.navigateTo({
+      url: '../question/question',
+    })
+  },
+  refresh:function(){
+    
+  }
 })
