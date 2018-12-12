424394e6c6fd7182b27d2784af9f143b495de8f8
jmAndroid <jay@huangjie.name>
2018-12-12 17:33:35 +0800

    加入视频和音频

--- data/data_index.js
+++ data/data_index.js
@@ -9,6 +9,7 @@ var index= {
     "id": 1,
         "data": [
         {
+            "type": 0,
             "question_id": 1,
             "answer_id": 3,
             "feed_source_id": 23,
@@ -21,6 +22,7 @@ var index= {
             "comment_num": "18"
         },
         {
+            "type": 0,
             "question_id": 2,
             "answer_id": 25,
             "feed_source_id": 24,
@@ -33,6 +35,7 @@ var index= {
             "comment_num": "18"
         },
         {
+            "type": 0,
             "question_id": 3,
             "answer_id": 61,
             "feed_source_id": 25,
@@ -45,18 +48,20 @@ var index= {
             "comment_num": "18"
         },
         {
+            "type": 1,
             "question_id": 4,
             "answer_id": 3,
             "feed_source_id": 23,
             "feed_source_name": "Rebecca",
             "feed_source_txt": "赞了回答4",
             "feed_source_img": "../../images/icon1.jpeg",
-            "question": "选择 Kindle 而不是纸质书的原因是什么？",
-            "answer_ctnt": "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
+            "question": "腾讯大学",
+          "answer_ctnt": "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
             "good_num": "112",
             "comment_num": "18"
         },
         {
+            "type": 0,
             "question_id": 5,
             "answer_id": 25,
             "feed_source_id": 24,
@@ -69,18 +74,23 @@ var index= {
             "comment_num": "18"
         },
         {
+            "type": 2,
             "question_id": 6,
             "answer_id": 61,
             "feed_source_id": 25,
             "feed_source_name": "George",
             "feed_source_txt": "赞了回答6",
             "feed_source_img": "../../images/icon9.jpeg",
-            "question": "气象铁塔的辐射大吗？",
-            "answer_ctnt": "我不知道那个铁塔的情况，不过气象铁塔上会有一些测太阳辐射的设备，如果说辐射的话，太阳辐射那么多，大家赶紧躲进地底下呀~~~~~要不然辐射量这么大，会变异的呀~~~~",
+            "author": "许巍",
+            "name":"此时此刻",
+            "display":"听首音乐放松一下~",
+            "poster": "http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000",
+            "src":"http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46",
             "good_num": "112",
             "comment_num": "18"
         },
         {
+            "type": 0,
             "question_id": 7,
             "answer_id": 3,
             "feed_source_id": 23,
@@ -93,6 +103,7 @@ var index= {
             "comment_num": "18"
         },
         {
+            "type": 0,
             "question_id": 8,
             "answer_id": 25,
             "feed_source_id": 24,
